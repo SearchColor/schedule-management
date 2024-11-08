e3eed55fb7037a962a4bd4337d9c31b637c9f3d0
searchcolor <qkrdydwo1022@gmail.com>
2024-11-08 16:06:26 +0900

    feat: add method
    add @Vaild

--- src/main/java/com/example/schedulemanagement/dto/UserRequestDto.java
+++ src/main/java/com/example/schedulemanagement/dto/UserRequestDto.java
@@ -1,13 +1,18 @@
 package com.example.schedulemanagement.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class UserRequestDto {
 
+    @Email
     private String email;
+
+    @NotEmpty (message = "필수 입력 요소 입니다.")
     private String name;
-    private String registration_date;
-    private String modification_date;
+
 }
